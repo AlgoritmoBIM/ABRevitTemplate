c2be2be7783ea4e2e6cd06a53f31e9da7522942a
AlgoritmoBIM <arq.emanuel.rv@gmail.com>
2023-12-03 19:37:24 -0600

    Add project files.

---
+++ Revit_Template/Cmd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+
+
+namespace AB_Revit_Template
+{
+    [Transaction(TransactionMode.ReadOnly)]
+    public class CmdShowMessage : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData,
+                                ref string message,
+                                ElementSet elements)
+        {
+            TaskDialog.Show("Title", "Hola Mundo");
+
+            return Result.Succeeded;
+        }
+    }
+}
